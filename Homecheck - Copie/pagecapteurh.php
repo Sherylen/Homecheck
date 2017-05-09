542355faf8947080a7b9e0b9746b020c413c2dc3
LaBelleGosse <iribarne.manon@gmail.com>
2017-05-09 12:19:38 +0200

    dossier html/css final

---
+++ Homecheck - Copie/pagecapteurh.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="POPUP.css" />
+        <script type="text/javascript" src="menu.js"></script>
+        <title>Pop up capteur</title>
+    </head>
+    <body>   
+
+
+<div id="carre"> 
+       
+  <h2> Capteur d'Humidité :</h2>
+  <a href="#" class="On">On</a>
+  <a href="#" class="Off">Off</a>
+  <img class="capteurh" src="image/humi.png" alt="Capteur d'humidité" />  
+  <a href="capteursParPiece.php"> <img class="croix" src="image/croix2.png" alt="fermeture" /> </a>
+  <a href="pagecapteurf.php"> <img class="flechedh" src="image/fleched.png" alt="switch" /> </a>
+  <a href="pagecapteurt.php"> <img class="flechegh" src="image/flecheg.png" alt="switch" /> </a>
+  <img class="grapheh" src="image/graphehumi.png" alt="Graphique d'humidité" />  
+   <ul id="menuh">
+
+
+        <li>
+
+                
+                <a href="#">Secondes</a>
+
+                <ul>
+
+                        <li><a href="#">Heures</a></li>
+
+                        <li><a href="#">Auj.</a></li>
+
+                        <li><a href="#">Semaines</a></li>
+
+                        <li><a href="#">Mois</a></li>
+
+                        <li><a href="#">Années</a></li>
+        </li>
+
+</ul>
+
+         </div>
+
+
+
+    </body>
+
+</html>
---
+++ Homecheck - Copie/pagecapteurt.php
@@ -0,0 +1,53 @@
+
+
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="POPUP.css" />
+        <script type="text/javascript" src="menu.js"></script>
+        <title>Pop up capteur</title>
+    </head>
+    <body>   
+
+<div id="carre"> 
+       
+  <h2> Capteur de Température :</h2>
+  <a href="#" class="On">On</a>
+  <a href="#" class="Off">Off</a>
+  <img class="capteurt" src="image/thermo.png" alt="Capteur de Temperature" />  
+  <a href="capteursParPiece.php"> <img class="croix" src="image/croix2.png" alt="fermeture" /> </a>
+  <a href="pagecapteurh.php"> <img class="flechedt" src="image/fleched.png" alt="fermeture" /> </a>
+  <a href="pagecapteurf.php"> <img class="flechegt" src="image/flecheg.png" alt="fermeture" /> </a>
+  <img class="graphet" src="image/graphetemp.jpg" alt="Graphique de temperature" />  
+   <ul id="menut">
+
+
+        <li>
+
+                
+                <a href="#">Secondes</a>
+
+                <ul>
+
+                        <li><a href="#">Heures</a></li>
+
+                        <li><a href="#">Auj.</a></li>
+
+                        <li><a href="#">Semaines</a></li>
+
+                        <li><a href="#">Mois</a></li>
+
+                        <li><a href="#">Années</a></li>
+        </li>
+
+</ul>
+
+        
+         </div>
+
+
+
+    </body>
+
+</html>
